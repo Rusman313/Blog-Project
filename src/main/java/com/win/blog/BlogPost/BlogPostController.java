8c844975a341be224a9a67dbfcd0aa3eedf7c7c1
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-23 08:40:32 -0400

    created blogPostController.java

---
+++ src/main/java/com/win/blog/BlogPost/BlogPostController.java
@@ -0,0 +1,12 @@
+package com.win.blog.BlogPost;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogPostController {
+    @GetMapping
+    public String index(BlogPost blogPost){
+        return "blogPost/index";
+    }
+}
